cdfe1add6b9b163e0e67550a26d4adb40962db59
Haoran Yu <haoranyu6@gmail.com>
2020-03-08 23:56:23 -0700

    Change the error city name

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -32,7 +32,7 @@ class _LocationScreenState extends State<LocationScreen> {
         temperature = 0;
         weatherIcon = 'Error';
         weatherMessage = 'Unable to get weather data';
-        cityName = '';
+        cityName = null;
         return;
       }
 
